454158ec12b836914b45fb6e95c1238b4a7a5c20
Sohaila Ali <ali.sohailakhalid@gmail.com>
2021-10-28 17:25:30 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main(){
+    static void Main(string[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to my game! ");
+        System.out.print("Enter a number: ");
+        int max_num = input.nextInt();
+
+        int random_int = ThreadLocalRandom.current().nextInt(0, max_num + 1);
+        int count = 0;
+        int attempt = 0;
+        System.out.println("--You'll have a maximum number of 3 guesses in total--");
+
+        while(true){
+            count += 1;
+            attempt += 1;
+            System.out.print("make a guess between 0 to " + max_num + " (Attepmt: " + attempt + "): ");
+            int guess = input.nextInt();
+
+            if (guess == random_num){
+                System.out.println("you've got it correct in " + count + " guesses! ");
+                break;    
+            } else if (count >= 3) {
+                System.out.println("Game over! ");
+                break;
+            } else if (guess > max_num){
+                System.out.println("*Enter a number below than " + max_num + "*");
+                count = 0;
+                attempt = 0;
+            } else {
+                System.out.println("you've got it incorrect:/ ");
+            }
+        }
+    }
+}
