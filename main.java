0113cfdd617ee4f4b6ccbf87549b10a12078a0ca
Sohaila Ali <ali.sohailakhalid@gmail.com>
2021-10-29 10:07:06 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Main(){
+public class Main{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to my game! ");
